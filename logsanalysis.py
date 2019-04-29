a67497556908070badd0ac7e40375dec73e39821
webblaster <yekinnijibola@gmail.com>
2019-04-29 18:39:40 +0100

    making changes after review

--- logsanalysis.py
+++ logsanalysis.py
@@ -9,13 +9,7 @@ cursor = db.cursor()
 
 # the most popular three articles of all time
 def top_three_popular_articles():
-    cursor.execute("""select articles.title, count(*) as views
-    from log, articles
-    where log.status = '200 OK'
-    and articles.slug = substr(log.path, 10)
-    group by articles.title
-    order by views desc
-    limit 3;""")
+    cursor.execute()
     results = cursor.fetchall()
     print "* The most popular three articles of all time are"
     for result in results:
@@ -24,13 +18,7 @@ def top_three_popular_articles():
 
 #the most popular article authors of all time
 def popular_authors():
-    cursor.execute("""select authors.name, count(*) as views
-    from log, authors, articles
-    where log.status = '200 OK'
-    and articles.slug = substr(log.path, 10)
-    and authors.id = articles.author
-    group by authors.name
-    order by views desc;""")
+    cursor.execute()
     results = cursor.fetchall()
     print ""
     print ""
@@ -40,13 +28,7 @@ def popular_authors():
 
 #day that led to more than one percent of errors
 def error_led_requests():
-    cursor.execute("""select errors.date,
-    errors.err/errors.total * 100 as percentage
-    from (select cast(time as date) as date,
-    count(*) as total,
-    cast(sum(cast(status != '200 OK' as int)) as float) as err
-    from log group by date) as errors
-    where errors.err/errors.total > 0.01;""")
+    cursor.execute()
     print ""
     print ""
     results = cursor.fetchall()
