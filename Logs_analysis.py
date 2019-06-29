5a2a2aa2068a084df933e0072b7b92c8c22023b0
webblaster <yekinnijibola@gmail.com>
2019-06-29 21:49:19 +0100

    refactored the code

---
+++ Logs_analysis.py
@@ -0,0 +1,64 @@
+#import psycopg2 module
+import psycopg2
+
+#Create database connection
+db = psycopg2.connect('dbname=news')
+cursor = db.cursor()
+
+###Sql queries to answer questions
+
+# sql to get the most popular three articles of all time
+first_sql = """select replace(log.path,'/article/',''),count(*) as num from log,articles 
+            where log.status='200 OK' and log.path=concat('/article/',articles.slug)
+            group by log.path order by num desc limit 3"""
+
+#sql to get the three most popular aritcle authors of all time
+second_sql = """ select authors.name,count(*) as num from log,articles,authors 
+            where log.status = '200 OK' and log.path=concat('/article/',articles.slug) and 
+            authors.id=articles.author group by authors.name order by num desc limit 3;"""
+
+#sql to get the day that led to more than one percent of errors
+#contains two views
+#err and req
+#create view err as select cast(time as date) as date,count(*) from log
+#where status !='200 OK' group by date order by count(*) desc;
+#create view req as select cast(time as date) as date,count(*) as num from log
+#group by date order by num desc;
+third_sql = """select err.date,(err.count*100.0/req.num) as perc from err join req on err.date=req.date 
+            where (err.count*100.0/req.num) > 1.0 order by req.num desc;"""
+### Define functions
+
+# the most popular three articles of all time
+def first_question():
+    cursor.execute(first_sql)
+    results = cursor.fetchall()
+    print "The most popular three articles of all time are"
+    for result in results:
+        print result[0], result[1],"--views"
+
+#the three most popular article authors of all time
+def second_question():
+    cursor.execute(second_sql)
+    results = cursor.fetchall()
+    print ""
+    print ""
+    print "The most popular three authors of all time are:"
+    for result in results:
+        print result[0] ,result[1],"--views"  
+
+#day that led to more than one percent of errors
+def third_question():
+    cursor.execute(third_sql)
+    print ""
+    print ""
+    results = cursor.fetchall()
+    for result in results:
+        print result[0],"--",result[1],"%","errors"
+
+
+#call the functions
+first_question()
+second_question()
+third_question()
+#close db connection
+db.close()      
--- readme.md
+++ readme.md
@@ -1,7 +1,14 @@
 LOGS ANALYSIS
 
-a project where you answer three questions with sql on a large database
+ABOUT
+a Udacity project where you answer three questions with sql on a large database
 the questions include:
 1-what are the top three most popular post of all time
 2-who are the top three authors that got the most views
 3-on which day did more than one percent of request led to errors
+
+SET UP
+
+VIEWS
+
+VIEW SETUP
