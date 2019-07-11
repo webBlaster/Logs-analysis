d253b830de7a9197215718417612a2fa11cc4798
Yekinni Ajibola <yekinnijibola@gmail.com>
2019-07-11 03:41:03 +0100

    pep8 adjustments

--- Logs_analysis.py
+++ Logs_analysis.py
@@ -1,27 +1,29 @@
-#import psycopg2 module
+#!/usr/bin/env python
+# import psycopg2 module
 import psycopg2
-#import datetime
+# import datetime
 from datetime import datetime
-#Create database connection
+# Create database connection
 db = psycopg2.connect('dbname=news')
 cursor = db.cursor()
 
-###Sql queries to answer questions
+# Sql queries to answer questions
 
 # sql to get the most popular three articles of all time
-first_sql = """select replace(log.path,'/article/',''),count(*) as num from log,articles 
-            where log.status='200 OK' and log.path=concat('/article/',articles.slug)
+first_sql = """select replace(log.path, '/article/', '') , count(*) as num from log, articles 
+            where log.status='200 OK' and log.path=concat('/article/', articles.slug)
             group by log.path order by num desc limit 3"""
 
-#sql to get the three most popular aritcle authors of all time
-second_sql = """ select authors.name,count(*) as num from log,articles,authors 
-            where log.status = '200 OK' and log.path=concat('/article/',articles.slug) and 
+# sql to get the three most popular aritcle authors of all time
+second_sql = """ select authors.name, count(*) as num from log, articles, authors 
+            where log.status = '200 OK' and log.path=concat('/article/', articles.slug) and 
             authors.id=articles.author group by authors.name order by num desc limit 3;"""
 
-#sql to get the day that led to more than one percent of errors
-third_sql = """select err.date,(err.count*100.0/req.num) as perc from err join req on err.date=req.date 
+# sql to get the day that led to more than one percent of errors
+third_sql = """select err.date, (err.count*100.0/req.num) as perc from err join req on err.date=req.date 
             where (err.count*100.0/req.num) > 1.0 order by req.num desc;"""
-### Define functions
+# Define functions
+
 
 # the most popular three articles of all time
 def first_question():
@@ -29,9 +31,10 @@ def first_question():
     results = cursor.fetchall()
     print "The most popular three articles of all time are"
     for result in results:
-        print result[0], result[1],"--views"
+        print result[0], result[1], "--views"
 
-#the three most popular article authors of all time
+
+# the three most popular article authors of all time
 def second_question():
     cursor.execute(second_sql)
     results = cursor.fetchall()
@@ -39,25 +42,26 @@ def second_question():
     print ""
     print "The most popular three authors of all time are:"
     for result in results:
-        print result[0] ,result[1],"--views"  
+        print result[0], result[1], "--views"  
 
-#day that led to more than one percent of errors
+
+# day that led to more than one percent of errors
 def third_question():
     cursor.execute(third_sql)
     print ""
     print ""
     results = cursor.fetchall()
-    #get date
+    # get date
     date_str = results[0][0]
-    #format date
+    # format date
     formated_date = date_str.strftime('%B %d %Y')
-    #output result
-    print formated_date,"--",results[0][1],"%","errors"
+    # output result
+    print formated_date, "--", results[0][1], "%", "errors"
 
 
-#call the functions
+# call the functions
 first_question()
 second_question()
 third_question()
-#close db connection
+# close db connection
 db.close()      
