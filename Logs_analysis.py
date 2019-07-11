977a507b09e4d968580e46f415e354a7b523e333
webblaster <yekinnijibola@gmail.com>
2019-07-11 15:38:32 +0100

    added more readme instructions

--- Logs_analysis.py
+++ Logs_analysis.py
@@ -1,27 +1,32 @@
-#import psycopg2 module
+# import psycopg2 module
 import psycopg2
-#import datetime
+# import datetime
 from datetime import datetime
-#Create database connection
+# Create database connection
 db = psycopg2.connect('dbname=news')
 cursor = db.cursor()
 
-###Sql queries to answer questions
+# Sql queries to answer questions
 
 # sql to get the most popular three articles of all time
-first_sql = """select replace(log.path,'/article/',''),count(*) as num from log,articles 
-            where log.status='200 OK' and log.path=concat('/article/',articles.slug)
+first_sql = """select replace(log.path, '/article/', ''),
+            count(*) as num from log, articles 
+            where log.status='200 OK' and
+            log.path=concat('/article/', articles.slug)
             group by log.path order by num desc limit 3"""
 
-#sql to get the three most popular aritcle authors of all time
-second_sql = """ select authors.name,count(*) as num from log,articles,authors 
-            where log.status = '200 OK' and log.path=concat('/article/',articles.slug) and 
-            authors.id=articles.author group by authors.name order by num desc limit 3;"""
+# sql to get the three most popular aritcle authors of all time
+second_sql = """ select authors.name,count(*) as num from log, articles, 
+            authors where log.status = '200 OK' and 
+            log.path=concat('/article/',articles.slug) and 
+            authors.id=articles.author group by authors.name 
+            order by num desc limit 3;"""
 
-#sql to get the day that led to more than one percent of errors
-third_sql = """select err.date,(err.count*100.0/req.num) as perc from err join req on err.date=req.date 
+# sql to get the day that led to more than one percent of errors
+third_sql = """select err.date,
+            (err.count*100.0/req.num) as perc from err join req on err.date=req.date 
             where (err.count*100.0/req.num) > 1.0 order by req.num desc;"""
-### Define functions
+# Define functions
 
 # the most popular three articles of all time
 def first_question():
@@ -29,7 +34,8 @@ def first_question():
     results = cursor.fetchall()
     print "The most popular three articles of all time are"
     for result in results:
-        print result[0], result[1],"--views"
+        print result[0], result[1], "--views"
+
 
 #the three most popular article authors of all time
 def second_question():
@@ -39,7 +45,8 @@ def second_question():
     print ""
     print "The most popular three authors of all time are:"
     for result in results:
-        print result[0] ,result[1],"--views"  
+        print result[0], result[1], "--views"  
+
 
 #day that led to more than one percent of errors
 def third_question():
@@ -52,7 +59,7 @@ def third_question():
     #format date
     formated_date = date_str.strftime('%B %d %Y')
     #output result
-    print formated_date,"--",results[0][1],"%","errors"
+    print formated_date, "--", results[0][1], "%", "errors"
 
 
 #call the functions
